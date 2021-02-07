43bf751e879e46d9cc171528a76651ccff7accbf
detailer <karan.arunverma@gmail.com>
2021-02-07 16:56:48 +0530

    Added Mongoose and MongoDB Connection

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { urlencoded, json } = require('express');
 
 const memeRoutes = require('./api/routes/memes');
 
+// Connecting to MongoDB
+mongoose.connect('mongodb+srv://firstSample:'
+    + process.env.MONGODB_ATLAS_PW
+    + '@cluster0.dcnnt.mongodb.net/testDB?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
 // Enable Logging 
 app.use(morgan('dev'));
+
+// Enable URL and JSON Parser
+app.use(urlencoded({extended: false}));
+app.use(json());
 
 // Handling CORS
 app.use((res, req, next) => {
