75b63ade79b8616c0f428e48f7e17b848d29e18e
detailer <karan.arunverma@gmail.com>
2021-02-07 16:43:26 +0530

    added CORS and basic error handling

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const memeRoutes = require('./api/routes/memes');
 
+// Enable Logging 
+app.use(morgan('dev'));
+
+// Handling CORS
+app.use((res, req, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 
+        'Origin, X-Requested, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        );
+        return res.status(200).json({});
+    }
+    next();
+ });
+
+// Route to /memes
 app.use('/memes', memeRoutes);
+
+// Handle Errors
+app.use((req, res, next) => {
+    const error = new Error('Not Found!');
+    error.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
 
 module.exports = app;
