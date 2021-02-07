d4fe49e768247d71806419d2978c9e86679ef0d2
detailer <karan.arunverma@gmail.com>
2021-02-07 17:09:18 +0530

    added GET routes

--- api/routes/memes.js
+++ api/routes/memes.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Meme = require('../models/meme');
 
 router.get('/', (req, res, next) => {
-    // TODO GET route for /memes
-    res.status(200).json({
-        message: 'Recieved at /memes'
+    Meme
+    .find()
+    .limit(100)
+    .select('name caption url')
+    .exec()
+    .then(result => {
+        if (!result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404);
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.get('/:memeId', (req, res, next) => {
-    // TODO GET route for /memesId
+    const id = req.params.memeId;
+    Meme
+    .findById(id)
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    });
 }); 
 
 router.post('/', (req, res, next) => {
