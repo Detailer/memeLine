e60d2fb701e3e2fd04bc0720a10c34f0171dced1
detailer <karan.arunverma@gmail.com>
2021-02-08 19:24:02 +0530

    added basic frontend

---
+++ frontend/res/script.js
@@ -0,0 +1,45 @@
+var memeContainer = document.getElementById('memeStream');
+
+function getMemes() {
+    fetch('http://localhost:8080/memes')
+    .then(res => res.json())
+    .then(data => {
+        data.reverse();
+        console.log(data);
+        for (var i = 0; i < data.length; i++){
+            var div = document.createElement('div');
+            var img = document.createElement('img');
+            img.setAttribute('id', 'memeImg');
+            div.innerHTML = '<b>Name</b>: ' + data[i].name + '<Br>' + '<b>Caption</b>: ' + data[i].caption;
+            img.src = data[i].url;
+            memeContainer.appendChild(div);
+            memeContainer.appendChild(img);
+        }
+    })
+    .catch(err => console.log(err));    
+}
+
+getMemes();
+
+document.getElementById('memeForm').onsubmit = function() {
+    var content = {
+        name : document.getElementById('name').value,
+        caption: document.getElementById('caption').value,
+        url: document.getElementById('url').value
+    };
+    console.log(content);
+    fetch('http://localhost:8080/memes', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(content)
+    })
+    .then(res => {
+        console.log(res);
+        memeContainer.innerHTML = "";
+        getMemes();
+    })
+    .catch(err => console.log(err));
+    return false;
+}
