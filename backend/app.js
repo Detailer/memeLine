9dee938d3b21d288b56af33ea4e7bb2b611aec32
detailer-asus <karan.arunverma@gmail.com>
2021-02-08 23:01:40 +0530

    Handed CORS to package

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const { urlencoded, json } = require('express');
+const cors = require('cors');
 
 const memeRoutes = require('./api/routes/memes');
 
@@ -21,19 +22,7 @@ app.use(urlencoded({extended: false}));
 app.use(json());
 
 // Handling CORS
-app.use((res, req, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 
-        'Origin, X-Requested, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods',
-            'PUT, POST, PATCH, DELETE, GET'
-        );
-        return res.status(200).json({});
-    }
-    next();
- });
+app.use(cors());
 
 // Route to /memes
 app.use('/memes', memeRoutes);
