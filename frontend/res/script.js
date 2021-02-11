c3ec5f0b99b0c000e7fd8bc88794453cd20174b6
detailer-asus <karan.arunverma@gmail.com>
2021-02-11 23:39:05 +0530

    added date on frontend

--- frontend/res/script.js
+++ frontend/res/script.js
@@ -1,6 +1,21 @@
 var memeContainer = document.getElementById('memeStream');
 var apiUrl = 'https://memeline.herokuapp.com/memes';
 
+// https://stackoverflow.com/questions/10346067/pull-date-from-mongo-id-on-the-client-side
+function getDateFromId(id){
+	// first 4 bytes are the timestamp portion (8 hex chars)
+	var timehex = id.substring(0,8);
+	console.log(timehex); // gives: 4f94c2a1
+
+	// convert to a number... base 16
+	var secondsSinceEpoch = parseInt(timehex, 16);
+	console.log(secondsSinceEpoch); // gives: 1335149217
+
+	// convert to milliseconds, and create a new date
+	var dt = new Date(secondsSinceEpoch*1000);
+	console.log(dt); // gives: Sun Apr 22 2012 22:46:57 GMT-0400 (EDT)
+	return dt;
+}
 
 function getMemes() {
 	fetch(apiUrl)
@@ -11,20 +26,24 @@ function getMemes() {
 				var singleMeme = document.createElement("div");
 				var memeCaption = document.createElement("div");
 				var memeUser = document.createElement("div");
+				var memeDate = document.createElement("div");
 				var memeImg = document.createElement("img");
 
 				singleMeme.setAttribute("class", "memeContainer shadow");
 				memeCaption.setAttribute("id", "memeCaption");
 				memeUser.setAttribute("id", "memeUser");
+				memeDate.setAttribute("id", "memeDate");
 				memeImg.setAttribute("id", "memeImg");
 
 				memeCaption.innerHTML = data[i].caption;
 				memeUser.innerHTML = "By " + data[i].name + "<hr>";
 				memeImg.src = data[i].url;
+				memeDate.innerHTML = getDateFromId(data[i].id);
 
 				singleMeme.appendChild(memeCaption);
 				singleMeme.append(memeUser);
 				singleMeme.appendChild(memeImg);
+				singleMeme.appendChild(memeDate);
 				memeContainer.appendChild(singleMeme);
 			}
 		})
