c0cb12e8a387bd9420f653ff9353877f6324c29b
detailer-asus <karan.arunverma@gmail.com>
2021-02-12 19:08:25 +0530

    added UI for edit button and modal

--- frontend/res/script.js
+++ frontend/res/script.js
@@ -20,14 +20,20 @@ function jsonToHtml(data) {
 	var memeUser = document.createElement("div");
 	var memeDate = document.createElement("div");
 	var memeImg = document.createElement("img");
+	var editButton = document.createElement("button");
 
 	singleMeme.setAttribute("class", "memeContainer shadow");
 	memeCaption.setAttribute("id", "memeCaption");
 	memeUser.setAttribute("id", "memeUser");
 	memeDate.setAttribute("id", "memeDate");
 	memeImg.setAttribute("id", "memeImg");
-
+	editButton.setAttribute("class", "editButton");
+	editButton.setAttribute("id", data.id);
+	editButton.setAttribute("onclick", "editClick(this)");
+	
 	memeCaption.innerHTML = data.caption;
+	editButton.innerHTML = "✎";
+	memeCaption.appendChild(editButton);
 	memeUser.innerHTML = "By " + data.name + "<hr>";
 	memeImg.src = data.url;
 	memeDate.innerHTML = getDateFromId(data.id);
@@ -76,3 +82,29 @@ document.getElementById('memeForm').onsubmit = function () {
 		.catch((err) => console.log(err));
 	return false;
 };
+
+// Modal Popup Functionality
+function editClick(button){
+	// Get the modal
+	var modal = document.getElementById("myModal");
+
+	// When the user clicks the button, open the modal 
+	modal.style.display = "block";
+	console.log(button.id);
+
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+	}
+}
